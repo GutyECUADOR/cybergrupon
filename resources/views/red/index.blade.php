2962fb1371ecb54f774fedef1d701e59929e1561
José L. Gutiérrez <gutiecuador@gmail.com>
2023-10-21 17:08:42 -0500

    Validacion 2do nivel

--- resources/views/red/index.blade.php
+++ resources/views/red/index.blade.php
@@ -63,75 +63,94 @@
                                                         @endif
                                                     </div>
                                                 </a>
-                                                <!-- Modal -->
-                                                <div class="modal fade" id="modal_position2_1" tabindex="-1" aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                        <h5 class="modal-title">Registrar en posición 2-1</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
+                                                @if(is_null($posicion2_1))
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="modal_position2_1" tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                            <h5 class="modal-title">Registrar en posición 2-1</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
 
-                                                            <form method="POST" action="{{ route('red.store') }}">
-                                                                @csrf
-                                                                <input type="hidden" class="form-control" name="id_usuario_location" value="{{ $posicion1_1->id }}" required>
-                                                                <input type="hidden" class="form-control" name="location" value="1" required>
+                                                                <form method="POST" action="{{ route('red.store') }}">
+                                                                    @csrf
+                                                                    <input type="hidden" class="form-control" name="id_usuario_location" value="{{ $posicion1_1->id }}" required>
+                                                                    <input type="hidden" class="form-control" name="location" value="1" required>
 
-                                                                <!-- Username -->
-                                                                <div class="mb-3">
-                                                                    <label for="nickname_promoter" class="form-label">Código de Promotor/Patrocinador </label>
-                                                                    <input type="text" id="nickname_promoter" class="form-control" name="nickname_promoter" value="{{ auth()->user()->nickname }}" readonly required>
-                                                                </div>
-
-                                                                <!-- Nickname -->
-                                                                <div class="mb-3">
-                                                                    <label for="nickname" class="form-label">Crea un nickname único (Sin espacios)</label>
-                                                                    <input type="nickname" name="nickname" value="{{old('nickname')}}" class="form-control" id="nickname" required autofocus>
-                                                                </div>
-
-                                                                <!-- Email -->
-                                                                <div class="mb-3">
-                                                                    <label for="email" class="form-label">Correo electrónico</label>
-                                                                    <input type="email" name="email" value="{{old('email')}}" class="form-control" id="email" required>
-                                                                </div>
-
-                                                                <!-- Phone -->
-                                                                <div class="mb-3">
-                                                                    <label for="phone" class="form-label">Teléfono</label>
-                                                                    <input type="phone" name="phone" value="{{old('phone')}}" class="form-control" id="phone" required>
-                                                                </div>
-
-                                                                <!-- Password -->
-                                                                <div class="mb-3">
-                                                                    <label for="password" class="form-label">Contraseña</label>
-                                                                    <input type="password" id="password" class="form-control" name="password" placeholder="" required>
-                                                                </div>
-
-                                                                <div class="mb-3">
-                                                                    <label for="password_confirmation" class="form-label">Confirme Contraseña</label>
-                                                                    <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" required>
-                                                                </div>
-
-                                                                <div>
-                                                                        <!-- Button -->
-                                                                        <div class="d-grid">
-                                                                    <button type="submit" class="btn btn-primary">
-                                                                        Crear nueva cuenta
-                                                                    </button>
+                                                                    <!-- Username -->
+                                                                    <div class="mb-3">
+                                                                        <label for="nickname_promoter" class="form-label">Código de Promotor/Patrocinador </label>
+                                                                        <input type="text" id="nickname_promoter" class="form-control" name="nickname_promoter" value="{{ auth()->user()->nickname }}" readonly required>
                                                                     </div>
-                                                                </div>
 
-                                                            </form>
+                                                                    <!-- Nickname -->
+                                                                    <div class="mb-3">
+                                                                        <label for="nickname" class="form-label">Crea un nickname único (Sin espacios)</label>
+                                                                        <input type="nickname" name="nickname" value="{{old('nickname')}}" class="form-control" id="nickname" required autofocus>
+                                                                    </div>
 
+                                                                    <!-- Email -->
+                                                                    <div class="mb-3">
+                                                                        <label for="email" class="form-label">Correo electrónico</label>
+                                                                        <input type="email" name="email" value="{{old('email')}}" class="form-control" id="email" required>
+                                                                    </div>
+
+                                                                    <!-- Phone -->
+                                                                    <div class="mb-3">
+                                                                        <label for="phone" class="form-label">Teléfono</label>
+                                                                        <input type="phone" name="phone" value="{{old('phone')}}" class="form-control" id="phone" required>
+                                                                    </div>
+
+                                                                    <!-- Password -->
+                                                                    <div class="mb-3">
+                                                                        <label for="password" class="form-label">Contraseña</label>
+                                                                        <input type="password" id="password" class="form-control" name="password" placeholder="" required>
+                                                                    </div>
+
+                                                                    <div class="mb-3">
+                                                                        <label for="password_confirmation" class="form-label">Confirme Contraseña</label>
+                                                                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" required>
+                                                                    </div>
+
+                                                                    <div>
+                                                                            <!-- Button -->
+                                                                            <div class="d-grid">
+                                                                        <button type="submit" class="btn btn-primary">
+                                                                            Crear nueva cuenta
+                                                                        </button>
+                                                                        </div>
+                                                                    </div>
+
+                                                                </form>
+
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                            </div>
                                                         </div>
-                                                        <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                                                         </div>
                                                     </div>
-                                                    </div>
-                                                </div>
+                                                @else
+                                                    <div class="modal" id="modal_position2_1" tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog modal-sm">
+                                                            <div class="modal-content ">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title">Posición no Válida</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <p>Posición ya está utilizada.</p>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Aceptar</button>
 
+                                                            </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endif
                                                 <ul>
                                                     <li>
                                                         <a href="#" data-bs-toggle="modal" data-bs-target="#modal_position3_1" style="margin-left: 30px;">
@@ -469,6 +488,7 @@
                                                     </div>
                                                 </a>
 
+                                                @if(is_null($posicion2_2))
                                                 <!-- Modal -->
                                                 <div class="modal fade" id="modal_position2_2" tabindex="-1" aria-hidden="true">
                                                     <div class="modal-dialog">
@@ -537,6 +557,25 @@
                                                     </div>
                                                     </div>
                                                 </div>
+                                                @else
+                                                    <div class="modal" id="modal_position2_2" tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog modal-sm">
+                                                            <div class="modal-content ">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title">Posición no Válida</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <p>Posición ya está utilizada.</p>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Aceptar</button>
+
+                                                            </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endif
 
                                                 <ul>
                                                     <li>
@@ -875,7 +914,7 @@
                                                         @endif
                                                     </div>
                                                 </a>
-
+                                                @if(is_null($posicion2_3))
                                                 <!-- Modal -->
                                                 <div class="modal fade" id="modal_position2_3" tabindex="-1" aria-hidden="true">
                                                     <div class="modal-dialog">
@@ -944,6 +983,25 @@
                                                     </div>
                                                     </div>
                                                 </div>
+                                                @else
+                                                    <div class="modal" id="modal_position2_3" tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog modal-sm">
+                                                            <div class="modal-content ">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title">Posición no Válida</h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <p>Posición ya está utilizada.</p>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Aceptar</button>
+
+                                                            </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endif
 
                                                 <ul>
                                                     <li>
