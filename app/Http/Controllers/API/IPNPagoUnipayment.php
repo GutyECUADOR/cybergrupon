dd80b616b234ee1c541d0190a5d5e01b0b61a427
José L. Gutiérrez <gutiecuador@gmail.com>
2023-11-19 21:05:41 -0500

    validacion de location ya asignada + register in logs

--- app/Http/Controllers/API/IPNPagoUnipayment.php
+++ app/Http/Controllers/API/IPNPagoUnipayment.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Compra;
 use App\Models\Pago;
+use App\Models\RecargaSaldo;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -39,12 +41,44 @@ class IPNPagoUnipayment extends Controller
      */
     public function store(Request $request)
     {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'invoice_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/log-AsignacionEnArbol.log'),
+              ])->info([$validator->errors(), $request->all()]);
+            return response([
+                'errors' => $validator->errors(),
+                'message' => 'Uno o más campos requeridos no pasaron la validación'
+            ], 400);
+        }
+
+
+
         $IPN_invoice = $request->all();
         $IPN_invoice_id = $request->get('invoice_id');
         $compra = Compra::where('orderID_gateway', $IPN_invoice_id)->firstOrFail();
         $user_compra = User::where('id', $compra->user_id)->firstOrFail(); // Usuario de la compra
         $user_promoter = User::where('nickname', '=', $user_compra->nickname_promoter)->first(); // promotor ejem: administrador
         $ID_Partner = $user_promoter->id;
+
+        $compra->status =  $IPN_invoice['status'];
+        $compra->save();
+
+        if ($user_compra->location || $user_compra->id_usuario_location) {
+            Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/log-AsignacionEnArbol.log'),
+              ])->info(['El usuario ya tiene posicion asignada', $request->all()]);
+            return response([
+                'errors' => 'El usuario ya tiene posicion asignada',
+                'message' => 'Uno o más campos requeridos no pasaron la validación'
+            ], 400);
+        }
 
         $location_free = $this->getLocation($ID_Partner);
         if($location_free->location > 3) {
@@ -56,8 +90,16 @@ class IPNPagoUnipayment extends Controller
         $user_compra->id_usuario_location = $location_free->id_usuario_location;
         $user_compra->save();
 
-        $compra->status =  $IPN_invoice['status'];
-        $compra->save();
+
+
+        RecargaSaldo::create([
+            'user_id' => $user_compra->id,
+            'valor' => $compra->valor,
+            'gateway' => 'UniPayment',
+            'orderID_interno' => $compra->orderID_interno,
+            'orderID_gateway' => $compra->orderID_gateway,
+            'status' => 'Payed',
+        ]);
 
         Log::build([
             'driver' => 'single',
