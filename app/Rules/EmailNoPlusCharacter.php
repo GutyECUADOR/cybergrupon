36006ee26e5de9c768990723d46994bbb7f8c730
José L. Gutiérrez <gutiecuador@gmail.com>
2024-01-16 14:06:41 -0500

    fix + character un email

---
+++ app/Rules/EmailNoPlusCharacter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class EmailNoPlusCharacter implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (str_contains($value, '+')) {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'No esta permitido el uso del caracter + en este registro.';
+    }
+}
